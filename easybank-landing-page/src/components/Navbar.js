eb1b25a8cbdf8ef9e38857af3b71c922ffcd2a0e
Mobeen67 <mobeenakhter67@gmail.com>
2021-03-28 16:01:19 +0500

    Final

--- easybank-landing-page/src/components/Navbar.js
+++ easybank-landing-page/src/components/Navbar.js
@@ -24,7 +24,7 @@ function Navbar() {
                         </Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/" className="nav-links">
+                    <Link to="about" className="nav-links">
                         About
                         </Link>
                 </li>
